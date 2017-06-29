bf3464b65be38b606e7be174deb89a0f9ac5ab68
Pavel <pavel@ucw.cz>
2017-06-29 13:31:01 +0200

    Get pulseaudio support back into working state.

--- utils/audio.c
+++ utils/audio.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 typedef int16_t s16;
 
@@ -108,6 +109,30 @@ float adjust_volume(float factor, s16 *b, int size, int dc_adj)
 #include "dsp.c"
 #endif
 
+static char wd_buf[1024] = "\0";
+
+char *wd_name(void)
+{
+	if (*wd_buf)
+		return wd_buf;
+
+	sprintf(wd_buf, "%s/herd/wd/call", getenv("HOME"));
+	return wd_buf;
+}
+
+void wd_write(char *s)
+{
+	int f = open(wd_name(), O_WRONLY | O_TRUNC | O_CREAT, 0600);
+
+	write(f, s, strlen(s));
+	close(f);
+}
+
+void wd_done(void)
+{
+	wd_write("no\n");
+}
+
 #define SSIZE (16*1024)
 char sbuf[SSIZE*2];
 
@@ -148,19 +173,28 @@ ssize_t audio_write(audio_t fd, void *buf, size_t count)
 	return res/2;
 }
 #else
-#error Update me: auto gain support, watchdog ....
 ssize_t audio_read(audio_t fd, void *buf, size_t count)
 {
 	ssize_t res = audio_read_raw(fd, buf, count);
-	adjust_volume(1, buf, count);
+	static float gain = 1;
+	gain = adjust_volume(1, buf, count, 0);
 	return res;
 }
 
 ssize_t audio_write(audio_t fd, void *buf, size_t count)
 {
 	ssize_t res;
-	adjust_volume(1, buf, count);
+	static float gain = 1;
+	gain = adjust_volume(1, buf, count, 0);
 	res = audio_write_raw(fd, buf, count);
+	{
+		char buf[1024];
+
+		sprintf(buf, "call\ndriver: %s\nspeaker: %.2f\n",
+			DRIVER_NAME,
+			gain);
+		wd_write(buf);
+	}
 	return res;
 }
 #endif
@@ -173,26 +207,3 @@ ssize_t audio_generate(s16 *buf, size_t count)
 	}
 }
 
-static char wd_buf[1024] = "\0";
-
-char *wd_name(void)
-{
-	if (*wd_buf)
-		return wd_buf;
-
-	sprintf(wd_buf, "%s/herd/wd/call", getenv("HOME"));
-	return wd_buf;
-}
-
-void wd_write(char *s)
-{
-	int f = open(wd_name(), O_WRONLY | O_TRUNC | O_CREAT, 0600);
-
-	write(f, s, strlen(s));
-	close(f);
-}
-
-void wd_done(void)
-{
-	wd_write("no\n");
-}
